a11ee1ebf8afd92b00e58fa55d3e2d490174facb
Albert Goncalves <goncalves.albert@gmail.com>
2021-12-05 18:08:40 -0500

    day 5 -> part 2 done

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,5 +1,5 @@
 import Data.Char (isDigit)
-import Data.Map (Map, fromListWith, toList)
+import Data.Map (Map, elems, fromListWith)
 import Text.ParserCombinators.ReadP
   ( ReadP,
     char,
@@ -33,22 +33,26 @@ parse :: String -> [Line]
 parse = fst . head . readP_to_S (lines <* eof)
 
 span :: Int -> Int -> [Int]
-span x0 x1 = if x0 < x1 then [x0 .. x1] else [x1 .. x0]
-
-intoCoords :: Line -> [Coord]
-intoCoords (Line (Coord x0 y0) (Coord x1 y1))
-  | x0 == x1 = map (Coord x0) $ span y0 y1
-  | y0 == y1 = map (`Coord` y0) $ span x0 x1
-  | otherwise = []
+span x0 x1
+  | x0 < x1 = [x0 .. x1]
+  | x1 < x0 = [x0, (x0 - 1) .. x1]
+  | otherwise = repeat x0
 
 intoMap :: [Coord] -> Map Coord Int
 intoMap xs = fromListWith (+) $ zip xs (repeat 1)
 
-part1 :: [Line] -> Int
-part1 = length . filter ((1 <) . snd) . toList . intoMap . concatMap intoCoords
+part1 :: Line -> [Coord]
+part1 (Line (Coord x0 y0) (Coord x1 y1))
+  | x0 == x1 = map (Coord x0) $ span y0 y1
+  | y0 == y1 = map (`Coord` y0) $ span x0 x1
+  | otherwise = []
 
-inject :: ([Line] -> Int) -> String -> String
-inject f = show . f . parse
+part2 :: Line -> [Coord]
+part2 (Line (Coord x0 y0) (Coord x1 y1)) =
+  zipWith Coord (span x0 x1) (span y0 y1)
+
+inject :: (Line -> [Coord]) -> String -> String
+inject f = show . length . filter (1 <) . elems . intoMap . concatMap f . parse
 
 main :: IO ()
-main = interact (\x -> unlines $ map (`inject` x) [part1])
+main = interact (\x -> unlines $ map (`inject` x) [part1, part2])
