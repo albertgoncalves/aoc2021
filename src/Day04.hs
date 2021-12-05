ba1147a142b0c1abf571d63e2efe3c092203cb94
Albert Goncalves <goncalves.albert@gmail.com>
2021-12-05 15:48:51 -0500

    day 4 -> part 1 done

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,5 +1,8 @@
 import Data.Char (isDigit)
-import Data.Map (Map, fromList)
+import Data.Function (on)
+import Data.List (groupBy, sort, sortBy)
+import Data.Map (Map, delete, fromList, keys, lookup)
+import Data.Maybe (mapMaybe)
 import Text.ParserCombinators.ReadP
   ( ReadP,
     char,
@@ -10,7 +13,7 @@ import Text.ParserCombinators.ReadP
     readP_to_S,
     sepBy1,
   )
-import Prelude hiding (sequence)
+import Prelude hiding (lookup, sequence)
 
 type Board = Map Int (Int, Int)
 
@@ -45,11 +48,41 @@ board = intoBoard <$> many1 (many1 (spaces *> integer) <* newline)
 boards :: ReadP [Board]
 boards = sepBy1 board newline
 
-game :: ReadP Game
-game = Game <$> (sequence <* newlines) <*> (boards <* eof)
+parse :: String -> Game
+parse =
+  fst . head . readP_to_S (Game <$> (sequence <* newlines) <*> (boards <* eof))
 
-solve :: String -> String
-solve = show . fst . head . readP_to_S game
+mark :: [Int] -> Board -> [(Int, Int)]
+mark xs b = mapMaybe (`lookup` b) xs
+
+isWinner :: [(Int, Int)] -> Bool
+isWinner xs = elem 5 $ map length $ rows ++ cols
+  where
+    xs' = sort xs
+    rows = groupBy ((==) `on` fst) xs'
+    cols = groupBy ((==) `on` snd) $ sortBy (compare `on` snd) xs'
+
+score :: [Int] -> Board -> Int
+score xs b = sum (keys $ foldr delete b xs) * last xs
+
+winners :: [Int] -> [Board] -> [(Board, [(Int, Int)])]
+winners xs bs = filter (isWinner . snd) $ zip bs (map (mark xs) bs)
+
+part1 :: Game -> Int
+part1 (Game xs bs) = head $ mapMaybe f [1 ..]
+  where
+    f n = case winners xs' bs of
+      [(b, _)] -> Just $ score xs' b
+      [] -> Nothing
+      _ -> undefined
+      where
+        xs' = take n xs
+
+part2 :: Game -> Int
+part2 = undefined
+
+inject :: (Game -> Int) -> String -> String
+inject f = show . f . parse
 
 main :: IO ()
-main = interact solve
+main = interact (\x -> unlines $ map (`inject` x) [part1, part2])
