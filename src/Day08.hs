5af9e3bb8bde65e465834affa668d88c52820c17
albertgoncalves <goncalves.albert@gmail.com>
2021-12-09 23:04:16 -0500

    cooler solution

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,6 +1,6 @@
-import Data.Bits (shift, (.|.))
 import Data.Char (isLower)
-import Data.List (delete, intersect, (\\))
+import Data.Function (on)
+import Data.List (sort, sortBy)
 import Data.Map (Map, fromList, (!))
 import Text.ParserCombinators.ReadP
   ( ReadP,
@@ -28,62 +28,45 @@ parse = fst . head . readP_to_S (many1 (line <* char '\n') <* eof)
 part1 :: ([String], [String]) -> Int
 part1 = length . filter (`elem` [2, 3, 4, 7]) . map length . snd
 
-ofLen :: Int -> [[a]] -> [[a]]
-ofLen n = filter ((== n) . length)
+segments :: [String]
+segments =
+  [ "abcefg",
+    "cf",
+    "acdeg",
+    "acdfg",
+    "bcdf",
+    "abdfg",
+    "abdefg",
+    "acf",
+    "abcdefg",
+    "abcdfg"
+  ]
 
-ofLen1 :: Int -> [[a]] -> [a]
-ofLen1 n = head . ofLen n
+reference :: Map String Int
+reference = fromList $ zip segments [0 .. 9]
 
-intersect1 :: Eq a => [a] -> [a] -> a
-intersect1 xs = head . intersect xs
+hash :: Char -> [String] -> [Int]
+hash x xs =
+  map snd $
+    filter (elem x . fst) $
+      sortBy (compare `on` snd) $
+        zip xs (map length xs)
 
-delete1 :: Eq a => a -> [a] -> a
-delete1 x = head . delete x
+keys :: String
+keys = "abcdefg"
 
-decode :: Map Char Int -> String -> Int
-decode m s =
-  case foldr ((.|.) . shift 1 . (m !)) 0 s :: Int of
-    119 -> 0
-    36 -> 1
-    93 -> 2
-    109 -> 3
-    46 -> 4
-    107 -> 5
-    123 -> 6
-    37 -> 7
-    127 -> 8
-    111 -> 9
-    _ -> undefined
+encoder :: [String] -> Map Char [Int]
+encoder xs = fromList $ map (\x -> (x, hash x xs)) keys
 
+decoder :: Map [Int] Char
+decoder = fromList $ map (\x -> (hash x segments, x)) keys
+
+translate :: [String] -> String -> Int
+translate xs = ((reference !) . sort) . map ((decoder !) . (encoder xs !))
+
+-- NOTE: See `https://www.reddit.com/r/haskell/comments/rbj981/advent_of_code_2021_day_08/hnpbpv2/`.
 part2 :: [String] -> [String] -> Int
-part2 xs = foldl1 (\x -> ((x * 10) +)) . map (decode m)
-  where
-    l2 = ofLen1 2 xs
-    l3 = ofLen1 3 xs
-    l4 = ofLen1 4 xs
-    l6 = ofLen 6 xs
-    l7 = ofLen1 7 xs
-
-    x0 = head $ l3 \\ l2
-    x2 = l2 `intersect` l3
-    x5 = x2
-
-    x1 = delete x0 (l4 \\ l3)
-    x3 = x1
-
-    x4 = concatMap (l7 \\) l6
-    x2' = intersect1 x2 x4
-    x3' = intersect1 x3 x4
-
-    x1' = delete1 x3' x1
-    x4' = delete1 x2' $ delete x3' x4
-    x5' = delete1 x2' x5
-    x6 = head $ foldr delete "abcdefg" [x0, x1', x2', x3', x4', x5']
-
-    m :: Map Char Int
-    m =
-      fromList
-        [(x0, 0), (x1', 1), (x2', 2), (x3', 3), (x4', 4), (x5', 5), (x6, 6)]
+part2 xs = foldl1 (\x -> ((x * 10) +)) . map (translate xs)
 
 inject :: (([String], [String]) -> Int) -> String -> String
 inject f = show . sum . map f . parse
