d24b1cf98e777cade7d6245813f7a83d26e06854
albertgoncalves <goncalves.albert@gmail.com>
2021-12-10 18:06:59 -0500

    day 9 -> part 2 done, could be faster?

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,7 +1,7 @@
 import Data.Char (isDigit, ord)
-import qualified Data.Map as M
+import Data.List (nub, sortBy)
+import Data.Map (Map, fromList, keys, lookup, (!))
 import Data.Maybe (mapMaybe)
-import qualified Data.Set as S
 import Text.ParserCombinators.ReadP
   ( ReadP,
     char,
@@ -27,33 +27,54 @@ parse = fst . head . readP_to_S (many1 digits <* eof)
 enumerate :: [a] -> [(Int, a)]
 enumerate = zip [0 ..]
 
-intoMap :: [[Int]] -> M.Map Coord Int
+intoMap :: [[Int]] -> Map Coord Int
 intoMap rows =
-  M.fromList
+  fromList
     [(Coord j i, x) | (i, row) <- enumerate rows, (j, x) <- enumerate row]
 
-check ::
-  M.Map Coord Int ->
-  Coord ->
-  (S.Set Coord, [Coord]) ->
-  (S.Set Coord, [Coord])
-check m x@(Coord j i) y@(s, xs)
-  | S.member x s = y
-  | all ((m M.! x) <) $ mapMaybe (`M.lookup` m) neighbors =
-    (S.union s $ S.fromList neighbors, x : xs)
-  | otherwise = y
-  where
-    neighbors =
-      [Coord (j - 1) i, Coord (j + 1) i, Coord j (i - 1), Coord j (i + 1)]
+neighbors :: Coord -> [Coord]
+neighbors (Coord j i) =
+  [Coord (j - 1) i, Coord (j + 1) i, Coord j (i - 1), Coord j (i + 1)]
 
-part1 :: [[Int]] -> Int
-part1 xs =
-  sum $ map ((+ 1) . (m M.!)) $ snd $ foldr (check m) (S.empty, []) (M.keys m)
+isLowest :: Map Coord Int -> Coord -> Bool
+isLowest m c = all (x <) $ mapMaybe (`lookup` m) (neighbors c)
   where
-    m = intoMap xs
+    x = m ! c
 
-inject :: ([[Int]] -> Int) -> String -> String
-inject f = show . f . parse
+lowest :: Map Coord Int -> [Coord]
+lowest m = filter (isLowest m) $ keys m
+
+part1 :: Map Coord Int -> Int
+part1 m = sum $ map ((+ 1) . (m !)) $ lowest m
+
+higherNeighbors :: Map Coord Int -> Coord -> [Coord]
+higherNeighbors m c =
+  map fst $
+    filter ((x <) . snd) $
+      mapMaybe sequence $
+        zip ns $
+          map (`lookup` m) ns
+  where
+    ns = neighbors c
+    x = m ! c
+
+searchUp :: Map Coord Int -> Coord -> [Coord]
+searchUp m c =
+  case lookup c m of
+    Nothing -> []
+    Just 9 -> []
+    _ -> c : concatMap (searchUp m) (higherNeighbors m c)
+
+part2 :: Map Coord Int -> Int
+part2 m =
+  product $
+    take 3 $
+      sortBy (flip compare) $
+        map (length . nub . searchUp m) $
+          lowest m
+
+inject :: (Map Coord Int -> Int) -> String -> String
+inject f = show . f . intoMap . parse
 
 main :: IO ()
-main = interact (\x -> unlines $ map (`inject` x) [part1])
+main = interact (\x -> unlines $ map (`inject` x) [part1, part2])
