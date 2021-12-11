822399e15b942ff6bf93e447f77351f1d9fd1bf0
albertgoncalves <goncalves.albert@gmail.com>
2021-12-10 23:40:26 -0500

    re-factor

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,20 +1,23 @@
+import Control.Monad (foldM)
 import Data.Either (lefts, rights)
 import Data.List (foldl', sort)
 
-check :: String -> String -> Either Char String
-check ys [] = Right ys
-check ('(' : ys) (')' : xs) = check ys xs
-check ('[' : ys) (']' : xs) = check ys xs
-check ('{' : ys) ('}' : xs) = check ys xs
-check ('<' : ys) ('>' : xs) = check ys xs
-check _ (x@')' : _) = Left x
-check _ (x@']' : _) = Left x
-check _ (x@'}' : _) = Left x
-check _ (x@'>' : _) = Left x
-check ys (x : xs) = check (x : ys) xs
+check :: String -> Either Char String
+check = foldM f []
+  where
+    f :: String -> Char -> Either Char String
+    f ('(' : xs) ')' = Right xs
+    f ('[' : xs) ']' = Right xs
+    f ('{' : xs) '}' = Right xs
+    f ('<' : xs) '>' = Right xs
+    f _ ')' = Left ')'
+    f _ ']' = Left ']'
+    f _ '}' = Left '}'
+    f _ '>' = Left '>'
+    f xs x = Right (x : xs)
 
 part1 :: [String] -> Int
-part1 = sum . map f . lefts . map (check [])
+part1 = sum . map f . lefts . map check
   where
     f :: Char -> Int
     f ')' = 3
@@ -23,29 +26,21 @@ part1 = sum . map f . lefts . map (check [])
     f '>' = 25137
     f _ = undefined
 
-resolve :: String -> String
-resolve [] = []
-resolve ('(' : xs) = ')' : resolve xs
-resolve ('[' : xs) = ']' : resolve xs
-resolve ('{' : xs) = '}' : resolve xs
-resolve ('<' : xs) = '>' : resolve xs
-resolve _ = undefined
-
 median :: Ord a => [a] -> a
 median xs = sort xs !! (length xs `div` (2 :: Int))
 
 part2 :: [String] -> Int
 part2 =
   median
-    . map (foldl' (\x -> ((x * 5) +)) 0 . map f . resolve)
+    . map (foldl' (\x -> ((x * 5) +)) 0 . map f)
     . rights
-    . map (check [])
+    . map check
   where
     f :: Char -> Int
-    f ')' = 1
-    f ']' = 2
-    f '}' = 3
-    f '>' = 4
+    f '(' = 1
+    f '[' = 2
+    f '{' = 3
+    f '<' = 4
     f _ = undefined
 
 inject :: ([String] -> Int) -> String -> String
